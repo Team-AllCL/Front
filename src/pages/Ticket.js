0235bd817b9e53ce997cddd90036ccd9e39abc1c
yunss719 <ys26@inu.ac.kr>
2025-03-31 20:45:13 +0900

    mk5

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -13,30 +13,16 @@ export default function Ticket() {
 
   return (
     <div className="ticket-wrapper">
-      <img
-        src="/images/logo.png"
-        alt="로고"
-        className="logo-button"
-        onClick={() => navigate('/main')}
-      />
+      <img src="/images/logo.png" alt="로고" className="logo-button" onClick={() => navigate('/main')} />
 
       <img src="/images/ticket.png" alt="티켓 배경" className="ticket-bg" />
-
-      <button className="buy-btn" onClick={() => setShowPopup(true)}>
-        티켓 구매하기
-      </button>
+      <button className="ticket-buy-btn" onClick={() => setShowPopup(true)}> 티켓 구매하기 </button>
 
       {showPopup && (
         <div className="popup-overlay">
           <div className="popup-img-wrapper">
-            <img
-              src="/images/ticket_popup.png"
-              alt="팝업 이미지"
-              className="popup-img"
-            />
-            <button className="popup-confirm-btn" onClick={handleConfirm}>
-              확인
-            </button>
+            <img src="/images/ticket_popup.png" alt="팝업 이미지" className="ticket-popup-img" />
+            <button className="popup-confirm-btn" onClick={handleConfirm}> 확인 </button>
           </div>
         </div>
       )}
